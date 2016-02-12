d74480df1128c94c16b837dca27eaed91c7496f4
Aliyah Jessa <aliyah@aliyahmaliyah.com>
2016-02-12 22:36:44 +0000

    adding content to the reddit

--- reddit.js
+++ reddit.js
@@ -39,13 +39,30 @@ Content.belongsToMany(User, {
 //This function will create a new user with Sequelize and call the callback once the user has been created
 
 function createNewUser(username, password, cb) {
-    User.create({username: username, password: password}).then(function(user) {
+    User.create({username: username, password: password}).then(function(user) { // then is saying "once you've done creating the user, then you're going to run the function on a user which is defined when the first function is called"
        cb(user); 
     });
     
 }
-createNewUser('aliyahmaliyah','blahbah', function(promisedUser) {
-  console.log('there he is!!!', promisedUser); 
+createNewUser('aliyahmaliyah','blahbah', function(promisedUser) { // this is the call back. you specify in THIS funciton what you want it to do
+  //console.log('there he is!!!', promisedUser); 
 });
 
-    
+//Write a function called createNewContent that takes a user ID, a URL, a title and a callback. This function will 
+//create a new Content item that is immediately associated with the user object passed to it. Once the content is created, 
+//your function will call the callback with that content.
+//Note that you will have to find the user by its ID before you can associate the content to the user.
+
+function createNewContent(userID, url, title, cb){
+    User.findById(userID).then(function(user){ // once you've found the userID I want you to create content associated with that user ID
+    user.createContent({
+        url: url,
+        title: title
+    }).then(function (content) {
+        cb(content)
+    })
+    });
+}
+createNewContent(1,'http://google.com', 'I heart google', function(content){
+    console.log(content);
+})
